dc728b931fa1c77962e66efa64a7f847f1dc5b6e
andreapaccagnella94 <andreapacca94@live.it>
2025-12-28 17:47:12 +0100

    Refactor password generation logic and update output message to include password length

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
 <?php
-// funzione per generare la password solo con il parametro del numero di caratteri iniziale
-function generaPassword($lunghezza = 16)
-{
-    // Definisci i caratteri: lettere, numeri e simboli
-    // divido nei vari gruppi
-    $numbers = "0123456789";
-    $lowercase = "abcdefghijklmnopqrstuvwxyz";
-    $uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $symbols = "!@#$%^&*()-_=+[]{}";
-    $caratteri = $numbers . $lowercase . $uppercase . $symbols;
-    $caratteriLunghezza = strlen($caratteri);
-    $stringaCasuale = '';
-
-    for ($i = 0; $i < $lunghezza; $i++) {
-        // Estrae un carattere casuale dalla stringa definita
-        $stringaCasuale .= $caratteri[rand(0, $caratteriLunghezza - 1)];
-    }
-
-    return $stringaCasuale;
-}
-
-// Esempio di utilizzo: genera una stringa di 20 caratteri
-// echo generaPassword(20);
-
-// mi creo una variabile di appoggio per il numero di caratteri e per la password
-$legnt_of_password = 0;
-$password = "";
-// recupero il parametro get dal form
-if (isset($_GET["password-lenght"])) {
-    // echo $_GET["password-lenght"]; // recuperato il dato dal form
-    $legnt_of_password = $_GET["password-lenght"];
-    $password = generaPassword($legnt_of_password);
-};
+require_once "./functions.php"
 
 
 ?>
@@ -119,7 +87,7 @@ if (isset($_GET["password-lenght"])) {
             <?php
             if ($password != "") {
             ?>
-                <h2>La tua password è:</h2>
+                <h2>La tua password di <?php echo $_GET["password-lenght"] ?> caratteri è:</h2>
                 <pre> <?php echo $password ?> </pre>
             <?php
             }
